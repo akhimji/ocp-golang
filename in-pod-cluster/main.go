677bb2ff92cddd16c8ced34af6c1a74c72890733
alyarctiq <aly.khimji@artiq.ca>
2020-12-05 20:34:20 -0500

    examples

---
+++ in-pod-cluster/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	buildv1client "github.com/openshift/client-go/build/clientset/versioned/typed/build/v1"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	corev1client "k8s.io/client-go/kubernetes/typed/core/v1"
+	"k8s.io/client-go/rest"
+)
+
+func main() {
+	// Build a rest.Config from configuration injected into the Pod by
+	// Kubernetes.  Clients will use the Pod's ServiceAccount principal.
+	restconfig, err := rest.InClusterConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	// If you need to know the Pod's Namespace, adjust the Pod's spec to pass
+	// the information into an environment variable in advance via the downward
+	// API.
+	namespace := os.Getenv("NAMESPACE")
+	if namespace == "" {
+		panic("NAMESPACE was not set")
+	}
+
+	// Create a Kubernetes core/v1 client.
+	coreclient, err := corev1client.NewForConfig(restconfig)
+	if err != nil {
+		panic(err)
+	}
+
+	// Create an OpenShift build/v1 client.
+	buildclient, err := buildv1client.NewForConfig(restconfig)
+	if err != nil {
+		panic(err)
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(rw http.ResponseWriter, req *http.Request) {
+		rw.Header().Set("Cache-Control", "no-store, must-revalidate")
+		rw.Header().Set("Content-Type", "text/plain")
+
+		// List all Pods in our current Namespace.
+		pods, err := coreclient.Pods(namespace).List(metav1.ListOptions{})
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Fprintf(rw, "Pods in namespace %s:\n", namespace)
+		for _, pod := range pods.Items {
+			fmt.Fprintf(rw, "  %s\n", pod.Name)
+		}
+
+		// List all Builds in our current Namespace.
+		builds, err := buildclient.Builds(namespace).List(metav1.ListOptions{})
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Fprintf(rw, "Builds in namespace %s:\n", namespace)
+		for _, build := range builds.Items {
+			fmt.Fprintf(rw, "  %s\n", build.Name)
+		}
+	})
+
+	// Run an HTTP server on port 8080 which will serve the pod and build list.
+	err = http.ListenAndServe(":8080", mux)
+	if err != nil {
+		panic(err)
+	}
+}
